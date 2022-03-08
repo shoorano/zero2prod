9f65162d05b8560dfd635379517f96adc8cb3df2
Shoorano <danwilloughby2@gmail.com>
2022-03-08 20:35:07 +0000

    added a new test route and amended import issue

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use tokio;
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn greet(req: HttpServer) -> impl Responder {
+async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", name)
+}
+
+async fn goodbye(req: HttpRequest) -> impl Responder {
+    let id = req.match_info().get("id").unwrap_or("World");
+    format!("Goodbye {}!", id)
 }
 
 #[tokio::main]
@@ -11,7 +15,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to_get(greet))
+            .route("/hello/{name}", web::get().to(greet))
+            .route("/goodbye/{id}", web::get().to(goodbye))
     })
     .bind("127.0.0.1:8000")?
     .run()
