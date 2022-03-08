a41f560f583bed7a9fabc9d85376b851d4965509
Shoorano <danwilloughby2@gmail.com>
2022-03-08 00:09:36 +0000

    added health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpServer) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to_get(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
