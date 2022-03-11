7689998815888af5d6960df5cd954ac0fd1fa557
Shoorano <danwilloughby2@gmail.com>
2022-03-11 23:36:39 +0000

    fixed rustfmt issues due to rust edition to 2021

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
@@ -7,12 +7,9 @@ async fn health_check() -> impl Responder {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .listen(listener)?
-    .run();
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
 
     Ok(server)
 }
