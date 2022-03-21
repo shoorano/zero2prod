6a2a89c08dccdcd86413c453244275788a9a0e26
Shoorano <danwilloughby2@gmail.com>
2022-03-21 20:12:36 +0000

    updated Cargo.toml to include sqlx crate, and refactored src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,29 @@ use actix_web::web::Form;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+/// End point to check server status.
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-#[derive(serde::Deserialize)]
+/// Struct that contains required information a user needs to provide to
+/// subscribe.
+#[derive(serde::Deserialize, Debug)]
 struct FormData {
     name: String,
     email: String,
 }
 
-async fn subscribe(_form: Form<FormData>) -> HttpResponse {
+/// Endpoint that receives urlencoded request data is then to be converted to
+/// FormData which utilises serde::Deserialize macro to return a 400 status
+/// HttpResponse if data cannot be converted to FormData
+async fn subscribe(form: Form<FormData>) -> HttpResponse {
+    println!("New subscriber: email: {}, name {}", form.email, form.name);
     HttpResponse::Ok().finish()
 }
 
+/// returns a Result, if Ok then returns a HttpServer, otherwise returns a
+/// std::io::Error
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
