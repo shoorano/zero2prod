1ed377debf522dc2cbfca34735e11f26fc4c0563
Shoorano <danwilloughby2@gmail.com>
2022-03-29 22:24:52 +0100

    added first insert query

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgConnection;
 
 /// Struct that contains required information a user needs to provide to
 /// subscribe.
@@ -11,7 +12,21 @@ pub struct FormData {
 /// Endpoint that receives urlencoded request data is then to be converted to
 /// FormData which utilises serde::Deserialize macro to return a 400 status
 /// HttpResponse if data cannot be converted to FormData
-pub async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    println!("New subscriber: email: {}, name {}", form.email, form.name);
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    _connection: web::Data<PgConnection>,
+) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,22 @@
 use crate::routes::{health_check, subscribe};
-
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 /// returns a Result, if Ok then returns a HttpServer, otherwise returns a
 /// std::io::Error
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    // Required as PgConnection is not cloneable and a HttpServer creates
+    // a copy of the closure for each of the host machines cores
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
