618c0a9e3e29873d605ec963cc1ef4bac8f5e485
Shoorano <danwilloughby2@gmail.com>
2022-04-09 11:18:36 +0100

    added trace span to src/routes/subscribers.rs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,12 +16,19 @@ pub struct FormData {
 /// HttpResponse if data cannot be converted to FormData
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
